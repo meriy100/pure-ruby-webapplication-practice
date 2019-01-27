a686cf1be8606596ff46d74d2f5f31b6256160b1
meriy100 <ttattataa@gmail.com>
2019-01-28 00:47:56 +0900

    [Modify] multiple thread

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
 require 'socket'
 
+def headers(client)
+  headers = []
+  while header = client.gets
+    break if header.chomp.empty?
+    headers << header.chomp
+  end
+  headers
+end
+
+def response(client)
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/plain"
+  client.puts
+  client.puts "message body"
+  client.close
+end
+
+
 if $PROGRAM_NAME == __FILE__
   server = TCPServer.new 3000
 
   loop do
-  client = server.accept
-    headers = []
-    while header = client.gets
-      break if header.chomp.empty?
-      headers << header.chomp
+    Thread.start(server.accept) do |client|
+      p [Thread.current]
+      m_headers = headers(client)
+      p [Thread.current, m_headers]
+      response(client)
     end
-    p headers
-
-    client.puts "HTTP/1.0 200 OK"
-    client.puts "Content-Type: text/plain"
-    client.puts
-    client.puts "message body"
-    client.close
   end
 end
